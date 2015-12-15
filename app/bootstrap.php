012fc836c904b728b57add159a03280f5bd11c9a
Georgiana Gligor <gb@tekkie.ro>
2015-12-15 16:25:44 +0200

    first request

---
+++ app/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$app = new Silex\Application;
+
+$app
+    ->match('/', function () { return 'Up and running.'; })
+    ->method('GET|POST');
+
+return $app;
