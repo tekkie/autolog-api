f65310096ac946140d56cda9459178bfaca214bf
Georgiana Gligor <gb@tekkie.ro>
2015-12-15 17:12:01 +0200

    logging with Monolog

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,10 +1,19 @@
 <?php
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+use Silex\Provider\MonologServiceProvider;
+
 $app = new Silex\Application;
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../logs/dev.log'
+));
 
 $app
-    ->match('/', function () { return 'Up and running.'; })
+    ->match('/', function () use ($app) {
+        $app['monolog']->addInfo('Logging example in the status route');
+        
+        return 'Up and running.';
+    })
     ->method('GET|POST');
 
 return $app;
